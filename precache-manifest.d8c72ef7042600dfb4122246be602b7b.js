30b10b874170ba2058df35138033010241ccd56e
Shuvo <meghsohor@yahoo.com>
2020-09-01 11:02:59 +0600

    Updates

---
+++ precache-manifest.d8c72ef7042600dfb4122246be602b7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a95ba9b8bc62c80c6ae2d8a0c4a6be5a",
+    "url": "/react-news-app-voice-assistant/index.html"
+  },
+  {
+    "revision": "ad82208ad90e890a036e",
+    "url": "/react-news-app-voice-assistant/static/css/main.6c894a9e.chunk.css"
+  },
+  {
+    "revision": "9c0f92bdde443cd62772",
+    "url": "/react-news-app-voice-assistant/static/js/2.05ad9016.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/react-news-app-voice-assistant/static/js/2.05ad9016.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad82208ad90e890a036e",
+    "url": "/react-news-app-voice-assistant/static/js/main.b32e1e46.chunk.js"
+  },
+  {
+    "revision": "8b80599eec5383f8bbf2",
+    "url": "/react-news-app-voice-assistant/static/js/runtime-main.89652de5.js"
+  }
+]);
